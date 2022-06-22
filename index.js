a381599a13478425b3863819f19324514ff9997e
Lithin87 <72386223+Lithin87@users.noreply.github.com>
2022-06-22 22:33:06 +0530

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const functions = require('@google-cloud/functions-framework');
 var axios = require('axios');
 
-functions.http('hello', (req, res) => {
+functions.http('hello',async (req, res) => {
 
   const host  = "https://sohanapp.azurewebsites.net";  
               
-  const response =  axios.get(host +"/api/HttpTrigger1");
-  console.log("LOG"+ response.data)
+  const response = await axios.get(host +"/api/HttpTrigger1");
+  // console.log("LOG :"+ (JSON.stringify(response.data)))
 
-
+  console.log("LOG :"+ JSON.stringify(response.data, null, 3));
   res.send(response.data);
 });
